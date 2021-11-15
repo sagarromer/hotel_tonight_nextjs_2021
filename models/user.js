6a8b48d3ce63619fb047164bfd5fe67488be786a
sagara dissanayake <39671491+sagarromer@users.noreply.github.com>
2021-11-14 20:03:12 -0800

    generate forget password token implemented

--- models/user.js
+++ models/user.js
@@ -58,4 +58,19 @@ userSchema.methods.comparePassword = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password)
 }
 
+// Generate password reset token
+userSchema.methods.getResetPasswordToken = function () {
+
+    // Generate token
+    const resetToken = crypto.randomBytes(20).toString('hex')
+
+    // Hash and set to resetPasswordToken field
+    this.resetPasswordToken = crypto.createHash('sha256').update(resetToken).digest('hex');
+
+    // Set token expire time
+    this.resetPasswordExpire = Date.now() + 30 * 60 * 1000
+
+    return resetToken;
+
+}
 export default mongoose.models.User || mongoose.model('User', userSchema)
